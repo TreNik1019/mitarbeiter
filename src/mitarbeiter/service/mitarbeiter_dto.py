1eaed5b6dacdf3353cd3c042d23a1c23ed24a722
Marcus Akay <anthony1007a@gmail.com>
2026-04-04 02:10:26 +0200

    feat: DTO-Klassen erstellt

---
+++ src/mitarbeiter/service/mitarbeiter_dto.py
@@ -0,0 +1,41 @@
+"""DTO-Klasse für Mitarbeiterdaten."""
+
+from dataclasses import dataclass
+from datetime import date
+from decimal import Decimal
+
+import strawberry
+
+from mitarbeiter.entity import Position, Geschlecht, Mitarbeiter
+
+__all__ = ["MitarbeiterDTO"]
+
+
+@dataclass(eq=False, slots=True, kw_only=True)
+@strawberry.type
+class MitarbeiterDTO:
+    """DTO für den Mitarbeiter."""
+
+    id: int
+    version: int
+    nachname: str
+    email: str
+    position: Position
+    gehalt: Decimal
+    eintrittsdatum: date
+    homepage: str | None
+    geschlecht: Geschlecht | None
+    username: str
+
+    def __init__(self, mitarbeiter: Mitarbeiter):
+        """DTO aus dem Mitarbeiter erstellt."""
+        self.id: int = mitarbeiter.id
+        self.version: int = mitarbeiter.version
+        self.nachname: str = mitarbeiter.nachname
+        self.email: str = mitarbeiter.email
+        self.position: Position = mitarbeiter.position
+        self.gehalt: Decimal = mitarbeiter.gehalt
+        self.eintrittsdatum: date = mitarbeiter.eintrittsdatum
+        self.homepage: str | None = mitarbeiter.homepage
+        self.geschlecht: Geschlecht | None = mitarbeiter.geschlecht
+        self.username: str = mitarbeiter.username
---
+++ src/mitarbeiter/service/werksausweis_dto.py
@@ -0,0 +1,24 @@
+"""DTO für Werksausweis ."""
+
+from dataclasses import dataclass
+from datetime import date
+
+import strawberry
+
+from mitarbeiter.entity import Ausweisstatus, Werksausweis
+
+
+@dataclass(eq=False, slots=True, kw_only=True)
+@strawberry.type
+class WerksausweisDTO:
+    """DTO-Klasse für den Werksausweis."""
+
+    status: Ausweisstatus
+    ausstellungsdatum: date
+    guthaben: float
+
+    def __init__(self, werksausweis: Werksausweis) -> None:
+        """Objekt wird aus einem Werksausweis ersellt."""
+        self.status = werksausweis.status
+        self.ausstellungsdatum = werksausweis.ausstellungsdatum
+        self.guthaben = werksausweis.guthaben
